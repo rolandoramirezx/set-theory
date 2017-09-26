5d08aae7d062ac02fcd03e049f96dac08c16f6b3
Rolando Ramirez <rolando.ramirezx@gmail.com>
2017-09-26 18:55:47 -0500

    added SetCharacterFactory and SetCharacterFactoryTest

---
+++ src/main/java/SetCharacterFactory.java
@@ -0,0 +1,56 @@
+import lombok.Data;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+import static org.apache.commons.lang3.math.NumberUtils.isParsable;
+
+public class SetCharacterFactory { //object-oriented design pattern
+
+    //should only ever be one fac
+
+    private static final SetCharacterFactory instance = new SetCharacterFactory();
+
+    public static SetCharacterFactory getInstance () {
+        return instance;
+    }
+
+    //Prevent others from creating this class
+    private SetCharacterFactory() {}
+
+    public SetCharacter<?> of(String value){
+        checkArgument(isNotBlank(value), "Value is required"); //Throw IllegalArgumentException
+        if (isParsable(value)){
+            if(isNumeric(value)){
+                return new IntegerCharacter(Integer.parseInt(value));
+            } else {
+                //Throw an exception to crash to program
+                throw new IllegalArgumentException("Value has to be integer value");
+            }
+        } else {
+            return new AlphaCharacter(value);
+        }
+    }
+
+    @Data
+    private class IntegerCharacter implements SetCharacter<Integer> {
+
+        private final int value;
+
+        @Override
+        public Integer getValue() {
+            return value;
+        }
+    }
+
+    @Data
+    private class AlphaCharacter implements SetCharacter<String> {
+
+        private final String value;
+
+        @Override
+        public String getValue() {
+            return value;
+        }
+    }
+}
---
+++ src/test/java/SetCharacterFactoryTest.java
@@ -0,0 +1,12 @@
+import static org.testng.Assert.*;
+
+public class SetCharacterFactoryTest {
+    @org.testng.annotations.Test
+    public void testGetInstance() throws Exception {
+    }
+
+    @org.testng.annotations.Test
+    public void testOf() throws Exception {
+    }
+
+}
