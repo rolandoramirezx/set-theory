b1a3a3dc5c3bd9dc1ce517ce042cde2b5456482a
Rolando Ramirez <rolando.ramirezx@gmail.com>
2017-10-02 21:02:50 -0500

    completed unit testing edits

--- src/test/java/SetCharacterFactoryTest.java
+++ src/test/java/SetCharacterFactoryTest.java
@@ -1,12 +1,58 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.testng.Assert.*;
 
 public class SetCharacterFactoryTest {
-    @org.testng.annotations.Test
+
+    @Test
     public void testGetInstance() throws Exception {
+        assertThat( SetCharacterFactory.getInstance() ).isNotNull();
     }
 
-    @org.testng.annotations.Test
-    public void testOf() throws Exception {
+    @Test
+    public void testIsSameInstance() throws Exception {
+        SetCharacterFactory lhs = SetCharacterFactory.getInstance();
+        SetCharacterFactory rhs = SetCharacterFactory.getInstance();
+        assertThat(lhs).isEqualTo(rhs);
     }
 
+    @DataProvider(name = "illegalArgumentExceptions")
+    public static Object[][] illegalArgumentExceptions() {
+        return new Object[][]{
+                {""}, //blank string
+                {null},
+                {"3.14"} //dealing with float numbers
+        };
+    }
+
+    @Test(dataProvider = "illegalArgumentExceptions")
+    public void testOfIllegalArgumentException(String value) throws Exception {
+        assertThatIllegalArgumentException().isThrownBy( () -> SetCharacterFactory.getInstance().of(value));
+    }
+
+    @DataProvider(name = "validValues") //provider method feeds the tested data
+    public static Object[][] Name() {
+        return new Object [][] {
+                {"A", SetCharacterFactory.AlphaCharacter.class, "A"},
+                {"1", SetCharacterFactory.IntegerCharacter.class, 1}
+        };
+    }
+
+    @Test(dataProvider = "validValues") //test method
+    public void testValidValues(String value, Class<?> clazz, Object expected) throws Exception {
+        assertThatCode(() -> {
+            SetCharacter character = SetCharacterFactory.getInstance().of(value);
+
+            //Check the type
+            assertThat(character.getClass()).isEqualTo(clazz);
+
+            //Check the value
+            assertThat(character.getValue()).isEqualTo(expected);
+
+        }).doesNotThrowAnyException();
+    }
 }
