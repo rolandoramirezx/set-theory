ac6cd0bffe75e1e708c94a938a3ac7ae6753234c
Rolando Ramirez <rolando.ramirezx@gmail.com>
2017-10-12 18:11:38 -0500

    Cleaned up code inserted by Git

--- src/test/java/ReflectiveSetTest.java
+++ src/test/java/ReflectiveSetTest.java
@@ -1,18 +1,11 @@
-import org.testng.Reporter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
-<<<<<<< HEAD
 import static org.assertj.core.api.Assertions.assertThat;
-=======
-import static org.assertj.core.api.Java6Assertions.assertThat;
->>>>>>> b6458cabb16e92f90145ad3da5074e0cb8c0160f
-import static org.testng.Assert.*;
 
 public class ReflectiveSetTest {
 
@@ -25,19 +18,15 @@ public class ReflectiveSetTest {
                 {"ABCDEFAABBCCDDEEFF", true},
                 {"ABAABA", false},
                 {"ABCDAABBCCAB", false},
-<<<<<<< HEAD
-=======
                 {"123411223344", true},
                 {"123411223344", true},
                 {"123411223334", false},
                 {"1A2B11AA22BB", true},
->>>>>>> b6458cabb16e92f90145ad3da5074e0cb8c0160f
         };
     }
 
     @Test(dataProvider = "reflexiveSets") //getting our data from reflexiveSets
     public void testProcess(String input, Boolean result) throws Exception {
-<<<<<<< HEAD
         Stream<String> characterStream = input.chars().mapToObj(i -> Character.toString((char) i));
         Collection<SetCharacter<?>> characters = characterStream.map(SetCharacterFactory.getInstance()::of).collect(toList());
 
@@ -53,14 +42,6 @@ public class ReflectiveSetTest {
 
         //assertThat(set.isValid()).isEqualTo(result);
 
-        assertThat(new ReflectiveSet(characters).process().isValid()).isEqualTo(result);
-
-
-=======
-        Stream<String> characterStream = input.chars().mapToObj(i -> Character.toString((char) i)); //converting input into coll of characters
-        Collection<SetCharacter<?>> characters = characterStream.map(SetCharacterFactory.getInstance()::of).collect(toList());
-
         assertThat(new ReflectiveSet(characters).process().isValid()).isEqualTo(result); //verifying whether isValid is in fact what our test says it should be
->>>>>>> b6458cabb16e92f90145ad3da5074e0cb8c0160f
     }
 }
